98f389a94bb6240466449764eb74a561a6b63e65
Baggiyam Shanmugam <gowribaggiyam@gmail.com>
2025-03-31 17:45:15 +0200

    Create OrderModel

---
+++ shopzyBackend/Models/OrderModel.js
@@ -0,0 +1,79 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+const Order = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    orderItems: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: 'Product'
+
+            },
+            name: {
+                type: String,
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+            price: {
+                type: Number,
+                required: true,
+            },
+            image: {
+                type: String,
+            },
+
+        }
+    ],
+    shippingAddress: {
+        address: { type: String, required: true },
+        city: { type: String, required: true },
+        postalCode: { type: String, required: true },
+        country: { type: String, required: true },
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+    },
+    paymentResult: {
+        id: String,
+        status: String,
+        update_time: String,
+        email_address: String,
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0,
+    },
+    isPaid: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    paidAt: {
+        type: Date,
+    },
+    isDelivered: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    deliveredAt: {
+        type: Date,
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Pending',
+    },
+}, { timestamps: true, })
+const order = mongoose.model("order", orderSchema)
+module.export = order;
